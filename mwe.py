71e422b0ef3e1f6bc8b9ab505a510b697402b85d
Bas Crezee <bascrezee@gmail.com>
2018-09-03 11:49:04 +0200

    add files

---
+++ mwe.py
@@ -0,0 +1,15 @@
+import iris
+import numpy as np
+import iris.plot as iplot
+import matplotlib.pyplot as plt
+
+print("Iris version: {0}".format(iris.__version__))
+print("Numpy version: {0}".format(np.__version__))
+testfile = './testdata.nc'
+cube = iris.load_cube(testfile)
+# Try out commenting out the below line
+#assert type(cube.data)==type(cube.data)
+cube = cube[:,0,0]
+plt.title('Plot of iris cube data')
+iplot.plot(cube)
+plt.show()
